9d44955c013b0420f59754d6942205564a67fced
JettJan <111859851+JettJan@users.noreply.github.com>
2023-03-15 05:34:48 -0500

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,23 @@
+
+
+noun = input("Enter a noun: ")
+pnoun = input("Enter a plural noun: ")
+verb = input("Enter a present tense verb: ")
+verb2 = input("Enter another present tense verb: ")
+bpart = input("Enter a plural body part: ")
+adj = input("Enter an adjective: ")
+pnoun2 = input("Enter another plural noun: ")
+adj2 = input("Enter another adjective: ")
+
+print(f"Today, every student has a computer small enough to fit into his {noun}. "
+      f"He can solve any math problem by simply pushing the computer's little {pnoun}. "
+      f"Computers can add, multiply, divide, and {verb}. "
+      f"They can also {verb2} better than a human. "
+      f"Some computers are {bpart}. "
+      f"Others have a/an {adj} screen that shows all kinds of {pnoun2} and {adj2} figures.")
+
+
+
+
+
+
